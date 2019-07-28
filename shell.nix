59e25c0978ed57b2ed48996a3bb1b11a479d4885
aiden01 <liangu@protonmail.com>
2019-07-28 00:18:50 -0400

    feat(nix): add alias to start ghcid

--- shell.nix
+++ shell.nix
@@ -1,12 +1,15 @@
-with import <nixpkgs> {};
 
-{
-  LispyEnv = stdenv.mkDerivation {
-    name = "LispyEnv";
-    buildInputs = [
-      ghc
-      cabal-install
-      haskellPackages.ghcid
-    ];
-  };
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+in stdenv.mkDerivation {
+  name = "LispyEnv";
+  buildInputs = with pkgs; [
+    ghc
+    cabal-install
+    haskellPackages.ghcid
+  ];
+  shellHook = ''
+   alias repl="ghcid --command=cabal v2-repl"
+  '';
 }
